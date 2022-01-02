8c15743496ff57aefdc9acf9c5aa7d9ff240c310
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-01-02 16:41:42 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+
+    function letsFocus(e){
+        console.log("Good!")
+    }
+
+    function letsBlur(e){
+        console.log("Hey! Eyes on me!")
+    }
+
+    return(
+        <button onFocus={letsFocus} onBlur={letsBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
